fe37af6e9a0f822abb54953d60570064c760b144
Avery Burne <burneav@gmail.com>
2022-03-16 22:25:17 -0400

    Added cancel Tokens in useEffect on in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Pokemon from './Pokemon'
 import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState(['aggron', 'luxray'])
+  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPage, setNextPage] = useState()
+  const [prevPage, setPrevPage] = useState()
+  const [loading, setLoading] = useState(true)
 
-  axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
-    setPokemon(res.data.results.map(p => p.name))
-  })
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPage, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPage(res.data.next)
+      setPrevPage(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPage])
+
+  if (loading) return "Loading..."
 
   return (
     <Pokemon pokemon={pokemon}/>
